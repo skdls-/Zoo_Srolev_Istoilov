c51037ba5e8fb48292460e71b2a396d14ccc3f2d
Симеон Ролев <skdls@abv.bg>
2014-11-03 20:04:30 +0200

    Animals Class and Tests

---
+++ animal.py
@@ -0,0 +1,35 @@
+from random import randint
+
+class Animal:
+
+	def __init__ (self, species, age, name, gender, weight, life_expectancy, is_vegetarian):
+
+		self.species = species
+		self.age = age
+		self.name = name
+		self. gender = gender
+		self. weight = weight
+		self.life_expectancy = life_expectancy
+		self.is_vegetarian = is_vegetarian
+
+	def grow(self, years, kilos):
+
+		self.age += years
+		self.weight += kilos
+
+	def eat(self, food):
+
+		self.weight += food
+
+	def dead(self):
+
+		random_int = randint(1, life_expectancy) / self.life_expectancy
+		chance_of_dying = self.age / self.life_expectancy
+		return random_int < chance_of_dying
+
+
+
+
+
+
+
---
+++ test_animal.py
@@ -0,0 +1,32 @@
+import unittest 
+from animal import Animal
+
+class TestAnimal(unittest.TestCase):
+
+	def setUp(self):
+
+		self.my_animal = Animal("cat", 5, "Cattie", "female", 4, 15, True)
+
+	def test_init(self):
+
+		self.assertEqual(self.my_animal.species, "cat")
+		self.assertEqual(self.my_animal.age, 5)
+		self.assertEqual(self.my_animal.name, "Cattie")
+		self.assertEqual(self.my_animal.gender, "female")
+		self.assertEqual(self.my_animal.weight, 4)
+		self.assertEqual(self.my_animal.life_expectancy, 15)
+		self.assertTrue(self.my_animal.is_vegetarian)
+
+	def test_grow(self):
+
+		self.my_animal.grow(2, 2)
+		self.assertEqual(self.my_animal.age, 7)
+		self.assertEqual(self.my_animal.weight, 6)
+
+	def test_eat(self):
+
+		self.my_animal.eat(5000)
+		self.assertEqual(self.my_animal.weight, 5004)
+
+if __name__ == '__main__':
+	unittest.main()
