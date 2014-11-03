3e8446d022626b80969cd43798f54e54cfc1f667
Симеон Ролев <skdls@abv.bg>
2014-11-03 20:15:11 +0200

    Add some pep8

--- test_animal.py
+++ test_animal.py
@@ -1,32 +1,33 @@
 import unittest 
 from animal import Animal
 
+
 class TestAnimal(unittest.TestCase):
 
-	def setUp(self):
+    def setUp(self):
 
-		self.my_animal = Animal("cat", 5, "Cattie", "female", 4, 15, True)
+        self.my_animal = Animal("cat", 5, "Cattie", "female", 4, 15, True)
 
-	def test_init(self):
+    def test_init(self):
 
-		self.assertEqual(self.my_animal.species, "cat")
-		self.assertEqual(self.my_animal.age, 5)
-		self.assertEqual(self.my_animal.name, "Cattie")
-		self.assertEqual(self.my_animal.gender, "female")
-		self.assertEqual(self.my_animal.weight, 4)
-		self.assertEqual(self.my_animal.life_expectancy, 15)
-		self.assertTrue(self.my_animal.is_vegetarian)
+        self.assertEqual(self.my_animal.species, "cat")
+        self.assertEqual(self.my_animal.age, 5)
+        self.assertEqual(self.my_animal.name, "Cattie")
+        self.assertEqual(self.my_animal.gender, "female")
+        self.assertEqual(self.my_animal.weight, 4)
+        self.assertEqual(self.my_animal.life_expectancy, 15)
+        self.assertTrue(self.my_animal.is_vegetarian)
 
-	def test_grow(self):
+    def test_grow(self):
 
-		self.my_animal.grow(2, 2)
-		self.assertEqual(self.my_animal.age, 7)
-		self.assertEqual(self.my_animal.weight, 6)
+        self.my_animal.grow(2, 2)
+        self.assertEqual(self.my_animal.age, 7)
+        self.assertEqual(self.my_animal.weight, 6)
 
-	def test_eat(self):
+    def test_eat(self):
 
-		self.my_animal.eat(5000)
-		self.assertEqual(self.my_animal.weight, 5004)
+        self.my_animal.eat(5000)
+        self.assertEqual(self.my_animal.weight, 5004)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
